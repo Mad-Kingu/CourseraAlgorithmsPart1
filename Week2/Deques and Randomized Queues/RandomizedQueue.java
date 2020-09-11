30dbe42eec339bf2c12b0f8da099bc3fdd6d362f
Alper Ayna <alper_ayna@hotmail.com>
2020-09-11 16:13:41 +0300

    Update RandomizedQueue.java

--- Week2/Deques and Randomized Queues/RandomizedQueue.java
+++ Week2/Deques and Randomized Queues/RandomizedQueue.java
@@ -1,15 +1,18 @@
+import edu.princeton.cs.algs4.StdRandom;
+
 import java.util.Iterator;
-import java.util.Random;
 
 public class RandomizedQueue<Item> implements Iterable<Item> {
 
     private Item[] array; // original array
-    private final int size;
+    private int size;
+    private int elementAmount;
 
     // construct an empty randomized queue
-    public RandomizedQueue(int size) {
+    public RandomizedQueue() {
+        elementAmount = 0; // for counting
+        size = 1; // default for meh
         array = (Item[]) new Object[size];
-        this.size = size;
         for (int i = 0; i < size; i++) {
             array[i] = null;
         }
@@ -17,21 +20,12 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // is the randomized queue empty?
     public boolean isEmpty() {
-        for (int i = 0; i < size; i++) {
-            if (array[i] != null)
-                return false;
-        }
-        return true;
+        return elementAmount == 0;
     }
 
     // return the number of items on the randomized queue
     public int size() {
-        int temp = 0;
-        for (int i = 0; i < size; i++) {
-            if (array[i] != null)
-                temp++;
-        }
-        return temp;
+        return elementAmount;
     }
 
     // add the item
@@ -39,19 +33,28 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         if (item == null)
             throw new IllegalArgumentException("You can not add null value!");
 
-        boolean inserted = false;
-        int pointer = 0;
-        while (pointer < size) {
-            if (array[pointer] == null) {
-                array[pointer++] = item;
-                inserted = true;
-                break;
+        // array is full
+        if (elementAmount == size) {
+            Item[] newArray = (Item[]) new Object[size * 2];
+
+            for (int i = 0; i < size * 2; i++) {
+                if (i < size)
+                    newArray[i] = array[i];
+                else
+                    newArray[i] = null;
             }
-            pointer++;
+            array = newArray;
+            size = size * 2;
         }
 
-        if (!inserted && pointer == size) {
-            throw new IndexOutOfBoundsException("Randomized Queue is full!");
+        int tempIndex = 0;
+        while (true) {
+            if (array[tempIndex] == null) {
+                array[tempIndex] = item;
+                elementAmount++;
+                break;
+            }
+            tempIndex++;
         }
     }
 
@@ -59,6 +62,21 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
     public Item dequeue() {
         if (isEmpty())
             throw new java.util.NoSuchElementException("Deque is empty!");
+
+        // split the size to the half
+        if (elementAmount < size / 4) {
+            int newSize = size / 2;
+            Item[] newArray = (Item[]) new Object[newSize];
+
+            int tempIndex = 0;
+            for (int i = 0; i < size; i++) {
+                if (array[i] != null)
+                    newArray[tempIndex++] = array[i];
+            }
+            array = newArray;
+            size = newSize;
+        }
+
         int localSize = size();
         int[] indexes = new int[localSize];
 
@@ -73,10 +91,10 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
             }
         }
 
-        Random rand = new Random();
-        int n = rand.nextInt(localIndex);
+        int n = StdRandom.uniform(localIndex);
         Item returnValue = array[indexes[n]];
         array[indexes[n]] = null;
+        elementAmount--;
 
         return returnValue;
     }
@@ -86,53 +104,46 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
         if (isEmpty())
             throw new java.util.NoSuchElementException("Deque is empty!");
 
-        int[] indexes = new int[size];
-        int temp = 0;
+        int localSize = size();
+        int[] indexes = new int[localSize];
+
+        for (int i = 0; i < localSize; i++) {
+            indexes[i] = -1;
+        }
+
+        int localIndex = 0;
         for (int i = 0; i < size; i++) {
             if (array[i] != null) {
-                indexes[i] = i;
-                temp++;
+                indexes[localIndex++] = i;
             }
         }
-        Random rand = new Random();
-        int n = rand.nextInt(temp);
+        int n = StdRandom.uniform(localIndex);
 
         return array[indexes[n]];
     }
 
     // return an independent iterator over items in random order
     public Iterator<Item> iterator() {
-        int sizeOfTempArray = size();
-        Item[] tempArray = (Item[]) new Object[sizeOfTempArray];
-
-        int tempIndex = 0;
-        for (int i = 0; i < size; i++) {
-            if (array[i] != null)
-                tempArray[tempIndex++] = array[i];
-        }
-
-        Random rand = new Random();
-        for (int i = tempArray.length - 1; i > 0; i--) {
-            int index = rand.nextInt(i + 1);
-            // Simple swap
-            Item a = tempArray[index];
-            tempArray[index] = tempArray[i];
-            tempArray[i] = a;
-        }
-
         return new Iterator<Item>() {
-            private int pointer = 0;
+            int tempAmount = elementAmount;
+            int tempIndex = 0;
 
             @Override
             public boolean hasNext() {
-                return pointer != sizeOfTempArray;
+                return tempAmount != 0;
             }
 
             @Override
             public Item next() {
                 if (!hasNext())
                     throw new java.util.NoSuchElementException("No more item left in the Randomized Queue!");
-                return tempArray[pointer++];
+                while (true) {
+                    if (array[tempIndex] != null) {
+                        tempAmount--;
+                        return array[tempIndex++];
+                    } else
+                        tempIndex++;
+                }
             }
 
             @Override
@@ -144,29 +155,28 @@ public class RandomizedQueue<Item> implements Iterable<Item> {
 
     // unit testing (required)
     public static void main(String[] args) {
-        RandomizedQueue<Integer> randomizedQueue = new RandomizedQueue<>(10);
+        RandomizedQueue<Integer> randomizedQueue = new RandomizedQueue<>();
         System.out.println("Unit test started!");
 
-        Random rand = new Random();
         for (int i = 0; i < 10; i++) {
-            int n = rand.nextInt(Integer.MAX_VALUE);
+            int n = StdRandom.uniform(Integer.MAX_VALUE);
             randomizedQueue.enqueue(n);
         }
 
+        System.out.println("Iteration started!");
         for (Integer s : randomizedQueue)
             System.out.println(s);
 
-        System.out.println("Next iterator!");
-        for (Integer s : randomizedQueue)
-            System.out.println(s);
+        System.out.println("Sample!");
+        System.out.println(randomizedQueue.sample());
 
         System.out.println("Next iterator!");
         for (int i = 0; i < 10; i++) {
             System.out.println(randomizedQueue.dequeue());
         }
+
+        System.out.println("Next iterator!");
+        for (Integer s : randomizedQueue)
+            System.out.println(s);
     }
 }
-
-
-
-
