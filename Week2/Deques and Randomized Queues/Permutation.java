c2e31af39a620b9b60b19501000abf2e3f5139ef
Alper Ayna <alper_ayna@hotmail.com>
2020-09-11 16:13:20 +0300

    Update Permutation.java

--- Week2/Deques and Randomized Queues/Permutation.java
+++ Week2/Deques and Randomized Queues/Permutation.java
@@ -1,21 +1,14 @@
 import edu.princeton.cs.algs4.StdIn;
 
-import java.util.MissingFormatArgumentException;
-
 public class Permutation {
     public static void main(String[] args) {
-        if (args.length < 1)
-            throw new MissingFormatArgumentException("1 integer argument value is missing!");
 
         int k = Integer.parseInt(args[0]);
 
-        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<String>(k * 2);
+        RandomizedQueue<String> randomizedQueue = new RandomizedQueue<String>();
 
-        String temp = StdIn.readLine();
-        String[] tempArray = temp.split(" ");
-
-        for (String s : tempArray) {
-            randomizedQueue.enqueue(s);
+        while (!StdIn.isEmpty()) {
+            randomizedQueue.enqueue(StdIn.readString());
         }
 
         for (int i = 0; i < k; i++) {
